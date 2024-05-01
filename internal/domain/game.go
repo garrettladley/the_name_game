5dfe1a575765059b085c2bf9e69077ae86639f50
garrettladley <ladley.g@northeastern.edu>
2024-05-01 00:27:14 -0400

    id generation optimizations

--- internal/domain/game.go
+++ internal/domain/game.go
@@ -1,12 +1,15 @@
 package domain
 
 import (
+	"bytes"
 	"math/rand"
 )
 
 type Player struct {
 	Name string `json:"name"`
 }
+
+const IDLength int = 7 // 3 runes 1 dash 3 runes
 
 type ID string
 
@@ -17,18 +20,25 @@ type Game struct {
 
 type Games map[ID]Game
 
-func NewID(length int) ID {
-	alphaNumericChars := []rune{
+func NewID() ID {
+	alphaNumericRunes := []rune{
 		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
 		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
 		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
 		'u', 'v', 'w', 'x', 'y', 'z',
 	}
 
-	var result string
+	numAlphaNumericRunes := 36
 
-	for range length {
-		result += string(alphaNumericChars[rand.Intn(len(alphaNumericChars))])
+	id := make([]byte, IDLength)
+	buffer := bytes.NewBuffer(id)
+
+	for i := range id {
+		if i == 3 { // midpoint, add dash
+			buffer.WriteRune('-')
+		} else {
+			buffer.WriteRune(alphaNumericRunes[rand.Intn(numAlphaNumericRunes)])
+		}
 	}
-	return ID(result)
+	return ID(buffer.String())
 }
