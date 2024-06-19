3263eed8a4d94139d9b43e9f85d2f3e10eadfc10
garrettladley <garrett.ladley14@gmail.com>
2024-06-19 12:26:46 -0400

    fix stuff

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,7 +2,9 @@ package server
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/gofiber/fiber/v2/middleware/cache"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	fsession "github.com/gofiber/fiber/v2/middleware/session"
 
@@ -24,6 +26,11 @@ func Setup() *fiber.App {
 		Level: compress.LevelBestSpeed,
 	}))
 
+	app.Use(cache.New(cache.Config{
+		Expiration:   30 * time.Minute,
+		CacheControl: true,
+	}))
+
 	routes(app)
 
 	utility(app)
@@ -36,16 +43,20 @@ func routes(app *fiber.App) {
 
 	app.Get("/", handlers.Home)
 
-	app.Post("/game/new", intoSessionedHandler(handlers.NewGame, store))
-	app.Get("/game/join", handlers.JoinGameForm)
-	app.Post("/game/join", intoSessionedHandler(handlers.JoinGame, store))
-	app.Get("/game/:game_id", intoSessionedHandler(handlers.Game, store))
-	app.Get("/game/:game_id/qr", handlers.JoinGameQrCode)
-	app.Get("/game/:game_id/join", intoSessionedHandler(handlers.JoinGameFromQrCode, store))
-	app.Get("/game/:game_id/post", intoSessionedHandler(handlers.PostGame, store))
-	app.Post("/game/:game_id/submit", intoSessionedHandler(handlers.Submit, store))
-	app.Post("/game/:game_id/end", intoSessionedHandler(handlers.EndGame, store))
-	app.Get("/game/:game_id/post", intoSessionedHandler(handlers.PostGame, store))
+	app.Route("/game", func(r fiber.Router) {
+		r.Post("new", intoSessionedHandler(handlers.NewGame, store))
+		r.Get("/join", handlers.JoinGameForm)
+		r.Post("/join", intoSessionedHandler(handlers.JoinGame, store))
+		r.Route("/:game_id", func(r fiber.Router) {
+			r.Get("/", intoSessionedHandler(handlers.Game, store))
+			r.Get("/qr", handlers.JoinGameQRCode)
+			r.Get("/join", intoSessionedHandler(handlers.JoinGameFromLink, store))
+			r.Get("/post", intoSessionedHandler(handlers.PostGame, store))
+			r.Post("/submit", intoSessionedHandler(handlers.Submit, store))
+			r.Post("/end", intoSessionedHandler(handlers.EndGame, store))
+			r.Get("/post", intoSessionedHandler(handlers.PostGame, store))
+		})
+	})
 }
 
 func utility(app *fiber.App) {
